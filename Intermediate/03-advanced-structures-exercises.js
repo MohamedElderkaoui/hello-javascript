d778e53ce8cabac7f13d138dfc317604e8e2bae9
MohamedElderkaoui <mderkaoui10@gmail.com>
2025-02-13 14:52:51 +0100

    ejecicio 03 y05

--- Intermediate/03-advanced-structures-exercises.js
+++ Intermediate/03-advanced-structures-exercises.js
@@ -5,20 +5,84 @@ Vídeo: https://www.twitch.tv/videos/2373300186?t=00h15m32s
 
 // 1. Utiliza map, filter y reduce para crear un ejemplo diferente al de la lección
 
-// 2. Dado un array de números, crea uno nuevo con dichos números elevados al cubo y filtra sólo los números pares
+const numbers = [2, 4, 6, 8, 10];
 
+const sumOfSquares = numbers
+    .map(num => num ** 2)
+    .filter(square => square % 2 === 0)
+    .reduce((acc, square) => acc + square, 0);
+
+console.log(sumOfSquares);
+
+// 2. Dado un array de números, crea uno nuevo con dichos números elevados al cubo y filtra sólo los números pares
+const numbersCubed = numbers
+    .map(num => num ** 3)
+    .filter(cube => cube % 2 === 0);
+
+console.log(numbersCubed);
 // 3. Utiliza flat y flatMap para crear un ejemplo diferente al de la lección
 
-// 4. Ordena un array de números de mayor a menor
+const matrix = [[1, 2], [3, 4], [5, 6]];
+const flatMatrix = matrix.flat();
+console.log(flatMatrix);
 
+const matrix2 = [[1, 2], [3, 4], [5, 6]];
+const flatMapMatrix = matrix2.flatMap(num => [num * 2]);
+console.log(flatMapMatrix);
+
+// 4. Ordena un array de números de mayor a menor
+const numbersDesc = numbers.sort((a, b) => b - a);
+console.log(numbersDesc);
 // 5. Dados dos sets, encuentra la unión, intersección y diferencia de ellos
 
+const setA = new Set([1, 2, 3, 4, 5]);
+const setB = new Set([4, 5, 6, 7, 8]);
+
+const union = new Set([...setA, ...setB]);
+const intersection = new Set([...setA].filter(x => setB.has(x)));
+const difference = new Set([...setA].filter(x => !setB.has(x)));
+
+console.log(union);
+console.log(intersection);
+console.log(difference);
+
 // 6. Itera los resultados del ejercicio anterior
+for (const value of union) {
+    console.log(value);
+}
+
+for (const value of intersection) {
+    console.log(value);
+}
+
+for (const value of difference) {
+    console.log(value);
+}
 
 // 7. Crea un mapa que almacene información se usuarios (nombre, edad y email) e itera los datos
 
+
+const usersMap = new Map([
+    ["Brais", { name: "Brais", age: 37, email: "braismoure@mouredev.com" }],
+    ["Moure", { name: "Moure", age: 30, email: "mouredev@gmail.com" }],
+    ["MoureDev", { name: "MoureDev", age: 25, email: "mouredev@outlook.com" }],
+    ["mohamed", { name: "mohamed", age: 27, email: "mderkaoui10@gmail.com" }],
+
+]);
+
+for (const [key, value] of usersMap) {
+    console.log(`Nombre: ${value.name}, Edad: ${value.age}, Email: ${value.email}`);
+}
+
 // 8. Dado el mapa anterior, crea un array con los nombres
 
-// 9. Dado el mapa anterior, obtén un array con los email de los usuarios mayores de edad y transfórmalo a un set
+const namesArray = [...usersMap.keys()];
+console.log(namesArray);
 
+// 9. Dado el mapa anterior, obtén un array con los email de los usuarios mayores de edad y transfórmalo a un set
+const emailsSet = new Set([...usersMap.values()].filter(user => user.age >= 18).map(user => user.email));
+console.log(emailsSet);
 // 10. Transforma el mapa en un objeto, a continuación, transforma el objeto en un mapa con clave el email de cada usuario y como valor todos los datos del usuario
+const usersObject = Object.fromEntries(usersMap);
+const usersByEmail = new Map(Object.entries(usersObject).map(([email, user]) => [email, user]));
+console.log(usersByEmail);
--- Intermediate/06-advanced-objects-classes-exercises.js
+++ Intermediate/06-advanced-objects-classes-exercises.js
@@ -3,22 +3,184 @@ Clase 3 - Objetos y Clases avanzadas (12/02/2025)
 Vídeo: https://www.twitch.tv/videos/2379412787?t=00h21m33s
 */
 
-// 1. Agregega una función al prototipo de un objeto
+// 1. Agregar una función al prototipo de un objeto
+function Persona(nombre) {
+    this.nombre = nombre;
+}
 
-// 2. Crea un objeto que herede de otro
+Persona.prototype.saludar = function() {
+    console.log(`Hola, soy ${this.nombre}`);
+};
 
-// 3. Define un método de instancia en un objeto
+const persona1 = new Persona("Juan");
+persona1.saludar();
 
-// 4. Haz uso de get y set en un objeto
+// 2. Crear un objeto que herede de otro
+const animal = {
+    hacerSonido: function() {
+        console.log("Sonido genérico de animal");
+    }
+};
 
-// 5. Utiliza la operación assign en un objeto
+const perro = Object.create(animal);
+perro.hacerSonido();
 
-// 6. Crea una clase abstracta
+// 3. Definir un método de instancia en un objeto
+const coche = {
+    marca: "Toyota",
+    encender: function() {
+        console.log(`${this.marca} está encendido.`);
+    }
+};
 
-// 7. Utiliza polimorfismo en dos clases diferentes
+coche.encender();
 
-// 8. Implementa un Mixin
+// 4. Uso de get y set en un objeto
+class Usuario {
+    #edad; // Propiedad privada
 
-// 9. Crea un Singleton
+    constructor(edad) {
+        this.#edad = edad;
+    }
 
-// 10. Desarrolla un Proxy
+    get edad() {
+        return this.#edad;
+    }
+
+    set edad(nuevaEdad) {
+        if (nuevaEdad > 0) {
+            this.#edad = nuevaEdad;
+        } else {
+            console.error("La edad debe ser un número positivo");
+        }
+    }
+}
+
+const usuario = new Usuario(25);
+console.log(usuario.edad); // 25
+usuario.edad = 30; // Asignando un nuevo valor
+console.log(usuario.edad); // 30
+usuario.edad = -5; // Error
+
+
+// 5. Utilizar Object.assign() en un objeto
+const objetoA = { a: 1, b: 2 };
+const objetoB = { c: 3, d: 4 };
+const objetoC = Object.assign({}, objetoA, objetoB);
+console.log(objetoC);
+
+// 6. Crear una clase abstracta
+class Figura {
+    constructor() {
+        if (new.target === Figura) {
+            throw new Error("No se puede instanciar una clase abstracta");
+        }
+    }
+
+    calcularArea() {
+        throw new Error("Método abstracto, debe ser implementado");
+    }
+}
+
+class Circulo extends Figura {
+    constructor(radio) {
+        super();
+        this.radio = radio;
+    }
+
+    calcularArea() {
+        return Math.PI * this.radio ** 2;
+    }
+}
+
+const circulo = new Circulo(5);
+console.log(circulo.calcularArea());
+
+// 7. Utilizar polimorfismo en dos clases diferentes
+class Animal {
+    hacerSonido() {
+        console.log("Sonido genérico");
+    }
+}
+
+class Perro extends Animal {
+    hacerSonido() {
+        console.log("Guau Guau!");
+    }
+}
+
+class Gato extends Animal {
+    hacerSonido() {
+        console.log("Miau Miau!");
+    }
+}
+
+const animales = [new Perro(), new Gato(), new Animal()];
+animales.forEach(animal => animal.hacerSonido());
+
+// 8. Implementar un Mixin
+const Volador = {
+    volar() {
+        console.log(`${this.nombre} está volando!`);
+    }
+};
+
+class Superheroe {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+}
+
+Object.assign(Superheroe.prototype, Volador);
+
+const superman = new Superheroe("Superman");
+superman.volar();
+
+// 9. Crear un Singleton
+const Singleton = (function() {
+    let instancia;
+
+    function crearInstancia() {
+        return { mensaje: "Soy la única instancia" };
+    }
+
+    return {
+        obtenerInstancia: function() {
+            if (!instancia) {
+                instancia = crearInstancia();
+            }
+            return instancia;
+        }
+    };
+})();
+
+const instancia1 = Singleton.obtenerInstancia();
+const instancia2 = Singleton.obtenerInstancia();
+console.log(instancia1 === instancia2);
+
+// 10. Desarrollar un Proxy
+const persona = {
+    nombre: "Carlos",
+    edad: 30
+};
+
+const handler = {
+    get(obj, prop) {
+        return prop in obj ? obj[prop] : "Propiedad no encontrada";
+    },
+    set(obj, prop, valor) {
+        if (prop === "edad" && valor < 0) {
+            console.log("Edad no puede ser negativa");
+        } else {
+            obj[prop] = valor;
+        }
+    }
+};
+
+const personaProxy = new Proxy(persona, handler);
+
+console.log(personaProxy.nombre);
+console.log(personaProxy.altura);
+personaProxy.edad = -5;
+personaProxy.edad = 35;
+console.log(personaProxy.edad);
